ea602e8a00ed9389bef456a7ed177f0be25196a3
Vidhyasg03 <49891297+Vidhyasg03@users.noreply.github.com>
2019-04-22 20:08:49 -0700

    Add files via upload
    #added first login file

---
+++ MyClass1.java
@@ -0,0 +1,60 @@
+package NewPackageVid;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyClass1 {
+	
+	    public static void main(String[] args) {
+	        // declaration and instantiation of objects/variables
+			//comment the above 2 lines and uncomment below 2 lines to use Chrome
+				        
+	     login();
+	        
+	    }
+	        
+	       public static void login()
+	       {
+	    	   System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+		    	
+		        String baseUrl = "http://demo.guru99.com/test/newtours/";
+		        String expectedTitle = "Welcome: Mercury Tours";
+		        String actualTitle = "";
+
+		        // launch Fire fox and direct it to the Base URL
+		        driver.get(baseUrl);
+
+		        // get the actual value of the title
+		        actualTitle = driver.getTitle();
+
+		        /*
+		         * compare the actual title of the page with the expected one and print
+		         * the result as "Passed" or "Failed"
+		         */
+		        if (actualTitle.contentEquals(expectedTitle)){
+		            System.out.println("Test Passed!");
+		        } else {
+		            System.out.println("Test Failed");
+		        }
+
+	       WebElement Name= driver.findElement(By.name("userName"));
+	       WebElement P1 = driver.findElement(By.name("password"));
+	       
+	       Name.click();
+	       Name.sendKeys("vidhyasg03@gmail.com");
+	       System.out.println("entered the name");
+	       
+	       P1.click();
+	       P1.sendKeys("test");
+	       System.out.println("entered the password");
+	       
+	     //close Fire fox
+	        driver.close();
+	       
+	       }
+	       
+	        
+	        
+	    
+
+	}
